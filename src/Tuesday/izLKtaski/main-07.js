828042a5982d1039ab4904563929eec56772e962
gem4ik <mgemskij@gmail.com>
2023-06-11 22:41:30 +0300

    не доделал 7ю домашку по нативке

--- src/Tuesday/izLKtaski/main-07.js
+++ src/Tuesday/izLKtaski/main-07.js
@@ -23,10 +23,10 @@
 
 //3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
 //truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
-function truncateString(str, num) {
-    return str.slice(num)
-}
-console.log(truncateString('qwertyqwerty', 2))
+// function truncateString(str, num) {
+//     return str.slice(0, str.length-num)
+// }
+// console.log(truncateString('qwertyqwerty',1))
 
 //4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка или не строка, то возвращает null.
 // getMinLengthWord("Всем студентам инкубатора желаю удачи.") => "Всем"
@@ -34,10 +34,30 @@ console.log(truncateString('qwertyqwerty', 2))
 // getMinLengthWord(123) => null
 // getMinLengthWord(true) => null
 // getMinLengthWord(undefined) => null
+// function getMinLengthWord(str){
+//     if (str === '' && typeof str === 'string') {
+//         return null
+//     } else {
+//         const strArr = str.trim().split(' ')
+//         let word = strArr[0]
+//         for (let i=1;i<strArr.length;i++) {
+//             if (strArr[i].length < word.length){
+//                 word = strArr[i]
+//             }
+//         }
+//         return word
+//     }
+// }
+//
+// console.log(getMinLengthWord("Привет меня зовут Миша йц "))
 
 //5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
 // setUpperCase("всем стУдентам инкуБатора Желаю удачИ") => "Всем Студентам Инкубатора Желаю Удачи!"
-
+// function setUpperCase(str){
+//     return str.toLowerCase().split(' ')
+// }
+//
+// console.log(setUpperCase('вСем стУдентам инКубатора ЖелАю уДачИ!'))
 // !!!!!!!!!!!!!!!!!!После решения 5 задач - поднимаем руку!!!!!!!!
 
 //6. Реализуйте функцию, котрая принимает параметрами строку и подстроку. Если все
